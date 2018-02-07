7dee1bee930bfcd48c07729d6b4669884e2f254f
Matteo Pedani <matteo@pedani.it>
2018-02-08 00:24:28 +0100

    Aggiunto sito_lama

---
+++ Sito_lama/unmak.utf8.it_files/unmak.utf8.it.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.2 local-build-20101024
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({reflection_0:new IWReflection({opacity:0.50,offset:1.00})});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('unmak.utf8.it_files/unmak.utf8.itMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');fixAllIEPNGs('Media/transparent.gif');applyEffects()}
