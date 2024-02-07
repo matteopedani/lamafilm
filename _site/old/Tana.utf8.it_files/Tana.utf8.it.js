ce9748455e71ad927c469bdecbd4db189e2a8184
matteopedani <matteo@pedani.it>
2024-02-07 20:36:04 +0100

    aggiustato sito

---
+++ _site/old/Tana.utf8.it_files/Tana.utf8.it.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.2 local-build-20101024
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({reflection_0:new IWReflection({opacity:0.50,offset:1.00})});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Tana.utf8.it_files/Tana.utf8.itMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');fixAllIEPNGs('Media/transparent.gif');applyEffects()}
