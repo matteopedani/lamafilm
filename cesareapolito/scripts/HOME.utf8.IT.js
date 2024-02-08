7ce18640409b08619c921a3b5d57cb148e2880bf
matteopedani <matteo@pedani.it>
2024-02-08 10:51:58 +0100

    Built site for gh-pages

---
+++ cesareapolito/scripts/HOME.utf8.IT.js
@@ -0,0 +1,41 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+// Created by iWeb 3.0.1 local-build-20100514
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('HOME.utf8.IT_files/HOME.utf8.ITMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id2');fixupIECSS3Opacity('id3');fixupIECSS3Opacity('id4');fixupIECSS3Opacity('id5');fixupIECSS3Opacity('id6');fixupIECSS3Opacity('id7');fixupIECSS3Opacity('id8');fixupIECSS3Opacity('id9');performPostEffectsFixups()}
+
+
+}
+/*
+     FILE ARCHIVED ON 22:16:35 Jan 28, 2011 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 21:48:28 Feb 07, 2024.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  exclusion.robots: 0.1
+  exclusion.robots.policy: 0.089
+  cdx.remote: 0.105
+  esindex: 0.011
+  LoadShardBlock: 103.049 (6)
+  PetaboxLoader3.datanode: 126.008 (7)
+  load_resource: 82.88
+  PetaboxLoader3.resolve: 34.837
+*/
